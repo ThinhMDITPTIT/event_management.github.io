fa6b201a70374f30c163d7c9b3f033bd068876aa
ThinhMD <thinhmditptit@gmail.com>
2021-03-13 22:45:11 +0700

    [ThinhMD] Initiate project Event Management

---
+++ app/src/main/java/com/ptit/eventmanagement/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ptit.eventmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("user");
+
+        myRef.setValue("Thinh MD!");
+    }
+}
